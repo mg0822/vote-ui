13bbd763f876df0f0e75613bf6f013dd2da7ad25
Megumi Yamaguchi <66114814+mg0822@users.noreply.github.com>
2020-08-19 16:21:01 +0900

    aaa

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"Nekodayo 🐺")
-option_b = os.getenv('OPTION_B', u"Inudayo 🐶")
+option_a = os.getenv('OPTION_A', u"Neko 🐺")
+option_b = os.getenv('OPTION_B', u"Inu 🐶")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
